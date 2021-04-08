21677599b1ddd65f02b91641da7946d7abaefde5
Filip Voska <filip.voska@infinum.com>
2021-04-08 14:27:42 +0200

    Implement CVA

--- src/app/components/start-rating/start-rating.component.ts
+++ src/app/components/start-rating/start-rating.component.ts
@@ -1,15 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'cvae-start-rating',
   templateUrl: './start-rating.component.html',
   styleUrls: ['./start-rating.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => StartRatingComponent),
+      multi: true,
+    }
+  ]
 })
-export class StartRatingComponent {
+export class StartRatingComponent implements ControlValueAccessor {
   @Input() numberOfStars!: number;
-  @Input() public disabled = false;
-  @Input() public rating = 0;
-  @Output() public ratingChange = new EventEmitter<number>();
+  public disabled = false;
+  public rating = 0;
+
+  public onChange: (value: number) => void = () => null;
+  public onTouched: () => void = () => null;
 
   public onStarClick(newRating: number): void {
     if (this.disabled) {
@@ -17,6 +27,23 @@ export class StartRatingComponent {
     }
 
     this.rating = newRating;
-    this.ratingChange.emit(newRating);
+    this.onTouched();
+    this.onChange(newRating);
+  }
+
+  public writeValue(rating: number): void {
+    this.rating = rating;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
   }
 }
